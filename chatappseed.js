11e63af7489ae3d88cf4dd1a1d3201883cbe5377
mschoenf <Marc.Schoenfeld@mlb.com>
2017-09-29 12:06:24 -0400

    Update seed

--- chatappseed.js
+++ chatappseed.js
@@ -16,6 +16,14 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
 });
+
+var sql = "CREATE TABLE `BAMTECHChat`.`Users` (`id` INT NOT NULL AUTO_INCREMENT,`username` VARCHAR(45) NOT NULL,`password` VARCHAR(45) NOT NULL,PRIMARY KEY (`id`),UNIQUE INDEX `id_UNIQUE` (`id` ASC),UNIQUE INDEX `username_UNIQUE` (`username` ASC));";
+con.query(sql, function(err, result, fields){
+	if(err){
+		console.log(err);
+	}
+	console.log(result);
+});
   
 for(var i = 0; i < usernames.length; i++){
 	var sql = "INSERT INTO Users (username, password) VALUES ('" + usernames[i] + "', '" + passwords[i] + "');";
